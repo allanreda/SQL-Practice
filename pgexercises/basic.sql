5351f9c4775ce900d11061397449b58f0a2674c1
allanreda <allanreda@hotmail.com>
2024-08-10 00:57:37 +0200

    Ny database og tables sat op til nye øvelser

---
+++ pgexercises/basic.sql
@@ -0,0 +1,6 @@
+-- How can you retrieve all the information from the cd.facilities table?
+select * from cd.facilities
+
+-- You want to print out a list of all of the facilities and their cost to members. 
+-- How would you retrieve a list of only facility names and costs?
+select
