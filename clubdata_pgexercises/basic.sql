dc569e3fe11ecc5801bb700fb69f88adc88578ba
allanreda <allanreda@hotmail.com>
2024-08-10 01:54:15 +0200

    Spørgsmål besvaret

--- clubdata_pgexercises/basic.sql
+++ clubdata_pgexercises/basic.sql
@@ -10,5 +10,39 @@ from cd.facilities;
 
 -- How can you produce a list of facilities that charge a fee to members?
 select * 
-from cd.facilities
-where membercost > 0
+from 
+	cd.facilities
+where 
+	membercost > 0
+
+-- How can you produce a list of facilities that charge a fee to members, 
+-- and that fee is less than 1/50th of the monthly maintenance cost? 
+-- Return the facid, facility name, member cost, and monthly maintenance of the facilities in question.
+select 
+	facid, 
+	name, 
+	membercost, 
+	monthlymaintenance
+from 
+	cd.facilities
+where 
+	membercost < (monthlymaintenance / 50) and 
+	membercost > 1;
+
+-- How can you produce a list of all facilities with the word 'Tennis' in their name?
+select * 
+from 
+	cd.facilities
+where 
+	name like '%Tennis%';
+
+-- How can you retrieve the details of facilities with ID 1 and 5? Try to do it without using the OR operator.
+select *
+from 
+	cd.facilities
+where 
+	facid in (1, 5);
+
+-- How can you produce a list of facilities, with each labelled as 'cheap' or 'expensive' depending on if their 
+-- monthly maintenance cost is more than $100? Return the name and monthly maintenance of the facilities in question.
+
