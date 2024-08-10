1eacb8b4e60dd8c7bdf5f61d4462542d0c1df81c
allanreda <allanreda@hotmail.com>
2024-08-10 11:47:46 +0200

    Spørgsmål besvaret

--- clubdata_pgexercises/joins_and_subqueries.sql
+++ clubdata_pgexercises/joins_and_subqueries.sql
@@ -9,3 +9,21 @@ on
 where 
 	mems.firstname = 'David' and 
 	mems.surname = 'Farrell';
+
+-- How can you produce a list of the start times for bookings for tennis courts, for the date '2012-09-21'? 
+-- Return a list of start time and facility name pairings, ordered by the time.
+select 
+	bks.starttime, 
+	fac.name
+from 
+	cd.bookings as bks
+inner join 
+	cd.facilities as fac
+on 
+	bks.facid = fac.facid
+where 
+	fac.name like '%Tennis Court%' and 
+	bks.starttime >= '2012-09-21' and 
+	bks.starttime < '2012-09-22'
+order by 
+	bks.starttime asc
