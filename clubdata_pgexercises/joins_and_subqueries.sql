9c4159daee9cb9caadea881898b6b7aa2cf52402
allanreda <allanreda@hotmail.com>
2024-08-11 17:57:17 +0200

    Spørgsmål besvaret

--- clubdata_pgexercises/joins_and_subqueries.sql
+++ clubdata_pgexercises/joins_and_subqueries.sql
@@ -81,3 +81,36 @@ where
 order by 
 	member, 
 	facility;
+
+-- How can you produce a list of bookings on the day of 2012-09-14 which will cost the member (or guest) more than $30? 
+-- Remember that guests have different costs to members (the listed costs are per half-hour 'slot'), 
+-- and the guest user is always ID 0. Include in your output the name of the facility, the name of the member formatted 
+-- as a single column, and the cost. Order by descending cost, and do not use any subqueries.
+select 
+	concat(mem.firstname, ' ', mem.surname) as member, 
+	fac.name as facility,
+	case when 
+	    mem.memid = 0 then bks.slots * fac.guestcost else
+	    bks.slots * fac.membercost
+	end as cost
+from 
+	cd.members as mem
+inner join 
+	cd.bookings as bks
+on 
+	mem.memid = bks.memid
+inner join 
+	cd.facilities as fac
+on 
+	bks.facid = fac.facid
+where 
+	bks.starttime > '2012-09-14' and 
+	bks.starttime < '2012-09-15' and
+	(case 
+		when mem.memid = 0 then bks.slots * fac.guestcost 
+		else bks.slots * fac.membercost
+	end) > 30
+order by 
+	cost desc;
+
+
