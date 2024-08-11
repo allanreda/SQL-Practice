f8fcdb126f51deec94dc65d35cf82f3484394eb1
allanreda <allanreda@hotmail.com>
2024-08-11 15:36:44 +0200

    Spørgsmål besvaret

--- clubdata_pgexercises/joins_and_subqueries.sql
+++ clubdata_pgexercises/joins_and_subqueries.sql
@@ -13,7 +13,7 @@ where
 -- How can you produce a list of the start times for bookings for tennis courts, for the date '2012-09-21'? 
 -- Return a list of start time and facility name pairings, ordered by the time.
 select 
-	bks.starttime, 
+	bks.starttime as start, 
 	fac.name
 from 
 	cd.bookings as bks
@@ -26,4 +26,19 @@ where
 	bks.starttime >= '2012-09-21' and 
 	bks.starttime < '2012-09-22'
 order by 
-	bks.starttime asc
+	bks.starttime asc;
+
+-- How can you output a list of all members who have recommended another member? Ensure that there are no duplicates in the list, 
+-- and that results are ordered by (surname, firstname).
+select distinct 
+	mem.firstname, 
+	mem.surname
+from 
+	cd.members as mem
+inner join 
+	cd.members as rec
+on 
+	mem.memid = rec.recommendedby
+order by 
+	mem.firstname, 
+	mem.surname
