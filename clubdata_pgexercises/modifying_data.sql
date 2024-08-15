47240596d686d40113580a7998d5de4c5315ec1f
allanreda <allanreda@hotmail.com>
2024-08-16 00:45:21 +0200

    Modifying data-kategori færdig

--- clubdata_pgexercises/modifying_data.sql
+++ clubdata_pgexercises/modifying_data.sql
@@ -31,7 +31,35 @@ where facid = 1;
 
 -- We want to increase the price of the tennis courts for both members and guests. 
 -- Update the costs to be 6 for members, and 30 for guests.
-update cd.facilities 
+update 
+	cd.facilities 
 	set membercost = 6,
 	    guestcost = 30
 where name like '%Tennis Court%'
+
+-- We want to alter the price of the second tennis court so that it costs 10% more than the first one. 
+-- Try to do this without using constant values for the prices, so that we can reuse the statement if we want to.
+update 
+	cd.facilities as fac
+	set membercost = (select membercost * 1.1 from cd.facilities where facid = 0), 
+	    guestcost = (select guestcost * 1.1 from cd.facilities where facid = 0)
+where 
+	fac.facid = 1;
+
+-- As part of a clearout of our database, we want to delete all bookings from the cd.bookings table. 
+-- How can we accomplish this?
+delete from 
+	cd.bookings;
+
+-- We want to remove member 37, who has never made a booking, from our database. How can we achieve that?
+delete from 
+	cd.members
+where 
+	memid = 37;
+
+-- In our previous exercises, we deleted a specific member who had never made a booking. 
+-- How can we make that more general, to delete all members who have never made a booking?
+delete from 
+	cd.members
+where 
+	memid not in (select memid from cd.bookings);
